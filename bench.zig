51a071f914bd4d12732e88770af94f83ccef4d14
Pascal S. de Kloe <pascal@quies.net>
2023-08-19 18:21:11 +0200

    URL interpretation benchmark

--- bench.zig
+++ bench.zig
@@ -16,29 +16,47 @@ pub fn main() !void {
     var prng = std.rand.DefaultPrng.init(32);
     const rand = prng.random();
 
-    var host: [17]u8 = undefined;
-    @memcpy(&host, "www.some-host.com");
-    rand.shuffle(u8, &host);
-
-    var seg1 = try allocator.dupe(u8, "path");
-    var seg2 = try allocator.dupe(u8, "to a");
-    var seg3 = try allocator.dupe(u8, "long-name");
-    var path = [_][]u8{ seg1, seg2, seg3 };
-    rand.shuffle(u8, seg1);
-    rand.shuffle(u8, seg2);
-    rand.shuffle(u8, seg3);
-    rand.shuffle([]u8, &path);
-
     var timer = try Timer.start();
-    const start = timer.lap();
+    {
+        var host: [17]u8 = undefined;
+        @memcpy(&host, "www.some-host.com");
+        rand.shuffle(u8, &host);
 
-    var n: usize = bench_count;
-    while (n != 0) : (n -= 1) {
-        var s = try zuri.newURL("imap", null, &host, null, &path, allocator);
-        mem.doNotOptimizeAway(s);
-        allocator.free(s);
+        var seg1 = try allocator.dupe(u8, "path");
+        var seg2 = try allocator.dupe(u8, "to a");
+        var seg3 = try allocator.dupe(u8, "long-name");
+        var path = [_][]u8{ seg1, seg2, seg3 };
+        rand.shuffle(u8, seg1);
+        rand.shuffle(u8, seg2);
+        rand.shuffle(u8, seg3);
+        rand.shuffle([]u8, &path);
+
+        const start = timer.lap();
+
+        var n: usize = bench_count;
+        while (n != 0) : (n -= 1) {
+            var s = try zuri.newURL("imap", null, &host, null, &path, allocator);
+            mem.doNotOptimizeAway(s);
+            allocator.free(s);
+        }
+
+        const end = timer.read();
+        try report.print("URL construction with host and {d}-segment path took {d} on average, including free\n", .{ path.len, @divTrunc(end - start, bench_count) });
     }
 
-    const end = timer.read();
-    try report.print("host with {d}-segment path took {d} ns per URL, including free\n", .{ path.len, @divTrunc(end - start, bench_count) });
+    {
+        const sample = "http://www.example.com/path/to%20a/long-name";
+
+        timer.reset();
+        const start = timer.lap();
+
+        var n: usize = bench_count;
+        while (n != 0) : (n -= 1) {
+            var parts = try zuri.parse(sample);
+            mem.doNotOptimizeAway(&parts);
+        }
+
+        const end = timer.read();
+        try report.print("parse {s} took {d} ns on average\n", .{ sample, @divTrunc(end - start, bench_count) });
+    }
 }
