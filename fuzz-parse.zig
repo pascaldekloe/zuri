9c255f164e0905b9fa58e5c1f48b939bab90c17e
Pascal S. de Kloe <pascal@quies.net>
2023-09-13 19:18:38 +0200

    path normalisation fuzzing

--- fuzz-parse.zig
+++ fuzz-parse.zig
@@ -5,6 +5,10 @@ const mem = std.mem;
 
 const Urview = @import("./Urview.zig");
 
+var buf: [1024]u8 = undefined;
+var fix = std.heap.FixedBufferAllocator.init(&buf);
+const allocator = fix.allocator();
+
 pub fn main() !void {
     // fetch fuzz input
     const stdin = std.io.getStdIn();
@@ -17,9 +21,12 @@ pub fn main() !void {
 
     defer if (fuzzFail) os.exit(1);
 
-    verifyConstraints(ur, fuzz_in);
-    if (!fuzzFail) verifyEscapeMatch(ur);
+    try verifyConstraints(ur, fuzz_in);
+    if (!fuzzFail) try verifyEscapeMatch(ur);
     mem.doNotOptimizeAway(ur.port());
+
+    allocator.free(try ur.pathNorm("", allocator));
+    allocator.free(try ur.pathNorm("👯", allocator));
 }
 
 var fuzzFail = false;
@@ -29,14 +36,13 @@ fn fail(comptime format: []const u8, args: anytype) void {
     std.log.err(format, args);
 }
 
-var buf: [1024]u8 = undefined;
-
 // VerifyConstraints checks the claims in field comments from Parts.
-fn verifyConstraints(ur: Urview, fuzz_in: []const u8) void {
+fn verifyConstraints(ur: Urview, fuzz_in: []const u8) !void {
     // lossless mapping
     {
         const components = .{ ur.raw_scheme, ur.raw_authority, ur.raw_path, ur.raw_query, ur.raw_fragment };
-        const rejoin = std.fmt.bufPrint(&buf, "{s}{s}{s}{s}{s}", components) catch "<fmt out of space>";
+        const rejoin = try std.fmt.allocPrint(allocator, "{s}{s}{s}{s}{s}", components);
+        defer allocator.free(rejoin);
         if (!mem.eql(u8, rejoin, fuzz_in))
             fail("raw components concatenated {s} does not equal original input {s}", .{ rejoin, fuzz_in });
     }
@@ -56,7 +62,8 @@ fn verifyConstraints(ur: Urview, fuzz_in: []const u8) void {
             fail("raw port {s} does not start with a colon character", .{ur.raw_port});
 
         const auth_components = .{ ur.raw_userinfo, ur.raw_host, ur.raw_port };
-        const reformat = std.fmt.bufPrint(&buf, "//{s}{s}{s}", auth_components) catch "<fmt out of space>";
+        const reformat = try std.fmt.allocPrint(allocator, "//{s}{s}{s}", auth_components);
+        defer allocator.free(reformat);
         if (!mem.eql(u8, reformat, ur.raw_authority))
             fail("raw authority components reformatted {s} do not equal raw authority {s}", .{ reformat, ur.raw_authority });
     } else {
@@ -77,36 +84,33 @@ fn verifyConstraints(ur: Urview, fuzz_in: []const u8) void {
 }
 
 // VerifyEscapeMatch requires a verifyConstraints pass.
-fn verifyEscapeMatch(ur: Urview) void {
-    var fix = std.heap.FixedBufferAllocator.init(&buf);
-    const allocator = fix.allocator();
-
-    var s = ur.scheme(allocator) catch "<scheme escape took too much memory>";
+fn verifyEscapeMatch(ur: Urview) !void {
+    var s = try ur.scheme(allocator);
     if (!ascii.eqlIgnoreCase(ur.raw_scheme[0 .. ur.raw_scheme.len - 1], s))
         fail("escaped scheme {s} does not equal raw scheme {s} in lower-case excluding colon", .{ s, ur.raw_scheme });
     allocator.free(s);
 
-    var u = ur.userinfo(allocator) catch "<userinfo escape took too much memory>";
+    var u = try ur.userinfo(allocator);
     if (ur.hasUserinfo(u) != (ur.raw_userinfo.len != 0))
         fail("escaped user {s} is not matched by raw userinfo {s}", .{ u, ur.raw_userinfo });
     allocator.free(u);
 
-    var h = ur.host(allocator) catch "<host escape took too much memory>";
+    var h = try ur.host(allocator);
     if (ur.hasHost(h) != (ur.raw_authority.len != 0))
         fail("escaped host {s} is not matched by raw {s}, authority {s}", .{ h, ur.raw_host, ur.raw_authority });
     allocator.free(h);
 
-    var p = ur.path(allocator) catch "<path escape took too much memory>";
+    var p = try ur.path(allocator);
     if (!ur.hasPath(p))
         fail("escaped path {s} is not matched by raw {s}", .{ p, ur.raw_path });
     allocator.free(p);
 
-    var q = ur.query(allocator) catch "<query escape took too much memory>";
+    var q = try ur.query(allocator);
     if (ur.hasQuery(q) != (ur.raw_query.len != 0))
         fail("escaped query {s} is not matched by raw {s}", .{ q, ur.raw_query });
     allocator.free(q);
 
-    var f = ur.fragment(allocator) catch "<fragment escape took too much memory>";
+    var f = try ur.fragment(allocator);
     if (ur.hasFragment(f) != (ur.raw_fragment.len != 0))
         fail("escaped fragment {s} is not matched by raw {s}", .{ f, ur.raw_fragment });
     allocator.free(f);
