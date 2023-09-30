03804bb1599cc120c06db5a59316b7fe476ba882
Pascal S. de Kloe <pascal@quies.net>
2023-09-30 14:50:05 +0200

    fuzz IDN

--- fuzz-urview.zig
+++ fuzz-urview.zig
@@ -24,6 +24,7 @@ pub fn main() !void {
     try verifyConstraints(ur, readb[0..readn]);
     if (!fuzzFail) try verifyEscapeMatch(ur);
     mem.doNotOptimizeAway(ur.hasDomainName());
+    mem.doNotOptimizeAway(ur.internationalDomainName(allocator));
     mem.doNotOptimizeAway(ur.portAsU16());
 
     allocator.free(try ur.pathNorm("", allocator));
